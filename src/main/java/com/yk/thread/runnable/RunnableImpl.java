b9c8071aa0be9dd9602242abc9dbc06a3351ca70
yk-sys <1749160721@qq.com>
2020-06-22 16:39:42 +0800

    Commit Message

--- src/main/java/com/yk/thread/runnable/RunnableImpl.java
+++ src/main/java/com/yk/thread/runnable/RunnableImpl.java
@@ -14,6 +14,7 @@ public class RunnableImpl implements Runnable {
 /** 2.在实现类中重写Runnable接口的run方法，设置线程任务*/
         for (int i = 0; i <20 ; i++) {
         System.out.println(Thread.currentThread().getName()+"----->"+i);
+            System.out.println("测试");
     }
 }
 }
